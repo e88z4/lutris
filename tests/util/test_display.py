e88z4/lutris

commit 0c88229acf02d56e7be5dd018bcd6665ac094af5

mypy: add typing annotations to util/display.py

lutris/util/display.py
@@ -4,7 +4,7 @@
 import enum
 import os
 import subprocess
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, List, Tuple, Union, Iterable, TYPE_CHECKING
 
 import gi
 
@@ -29,11 +29,14 @@ from gi.repository import Gdk, GLib, Gio, Gtk
 from lutris.util import cache_single
 from lutris.settings import DEFAULT_RESOLUTION_HEIGHT, DEFAULT_RESOLUTION_WIDTH
 from lutris.util.graphics.displayconfig import MutterDisplayManager
-from lutris.util.graphics.xrandr import LegacyDisplayManager, change_resolution, get_outputs
+from lutris.util.graphics.xrandr import LegacyDisplayManager, change_resolution, get_outputs, Output
 from lutris.util.log import logger
 
+if TYPE_CHECKING:
+    from lutris.gui.application import LutrisApplication
 
-def get_default_dpi():
+
+def get_default_dpi() -> int:
     """Computes the DPI to use for the primary monitor
     which we pass to WINE."""
     display = Gdk.Display.get_default()
@@ -47,7 +50,7 @@ def get_default_dpi():
 
 
 @cache_single
-def is_display_x11():
+def is_display_x11() -> bool:
     """True if"""
     display = Gdk.Display.get_default()
     return "x11" in type(display).__name__.casefold()
@@ -61,11 +64,11 @@ class DisplayManager:
         self.rr_config = GnomeDesktop.RRConfig.new_current(self.rr_screen)
         self.rr_config.load_current()
 
-    def get_display_names(self):
+    def get_display_names(self) -> List[str]:
         """Return names of connected displays"""
         return [output_info.get_display_name() for output_info in self.rr_config.get_outputs()]
 
-    def get_resolutions(self):
+    def get_resolutions(self) -> List[str]:
         """Return available resolutions"""
         resolutions = ["%sx%s" % (mode.get_width(), mode.get_height()) for mode in self.rr_screen.list_modes()]
         if not resolutions:
@@ -73,14 +76,14 @@ class DisplayManager:
             return ["%sx%s" % (DEFAULT_RESOLUTION_WIDTH, DEFAULT_RESOLUTION_HEIGHT)]
         return sorted(set(resolutions), key=lambda x: int(x.split("x")[0]), reverse=True)
 
-    def _get_primary_output(self):
+    def _get_primary_output(self) -> Optional[GnomeDesktop.RROutput]:
         """Return the RROutput used as a primary display"""
         for output in self.rr_screen.list_outputs():
             if output.get_is_primary():
                 return output
-        return
+        return None
 
-    def get_current_resolution(self):
+    def get_current_resolution(self) -> Tuple[str, str]:
         """Return the current resolution for the primary display"""
         output = self._get_primary_output()
         if not output:
@@ -90,7 +93,7 @@ class DisplayManager:
         return str(current_mode.get_width()), str(current_mode.get_height())
 
     @staticmethod
-    def set_resolution(resolution):
+    def set_resolution(resolution: Optional[Union[str, Iterable[Output]]]) -> None:
         """Set the resolution of one or more displays.
         The resolution can either be a string, which will be applied to the
         primary display or a list of configurations as returned by `get_config`.
@@ -99,7 +102,7 @@ class DisplayManager:
         return change_resolution(resolution)
 
     @staticmethod
-    def get_config():
+    def get_config() -> List[Output]:
         """Return the current display resolution
         This method uses XrandR and will not work on wayland
         The output can be fed in `set_resolution`
@@ -107,7 +110,7 @@ class DisplayManager:
         return get_outputs()
 
 
-def get_display_manager():
+def get_display_manager() -> Union[MutterDisplayManager, DisplayManager, LegacyDisplayManager]:
     """Return the appropriate display manager instance.
     Defaults to Mutter if available. This is the only one to support Wayland.
     """
@@ -226,7 +229,7 @@ def get_desktop_environment() -> Optional[DesktopEnvironment]:
     return DesktopEnvironment.UNKNOWN
 
 
-def _get_command_output(command):
+def _get_command_output(command: List[str]) -> Optional[bytes]:
     """Some rogue function that gives no shit about residing in the correct module"""
     try:
         return subprocess.Popen(  # pylint: disable=consider-using-with
@@ -234,6 +237,8 @@ def _get_command_output(command):
         ).communicate()[0]
     except FileNotFoundError:
         logger.error("Unable to run command, %s not found", command[0])
+
+    return None
 
 
 def is_compositing_enabled() -> bool:
@@ -263,7 +268,7 @@ def _check_compositor_active(command_set: Dict[str, Any]) -> bool:
     result = _get_command_output(command)
 
     if "active_result" in command_set:
-        return result == command_set["active_result"]
+        return bool(result == command_set["active_result"])
 
     return result != b""
 
@@ -276,7 +281,7 @@ _COMPOSITING_DISABLED_STACK = []
 
 
 @cache_single
-def _get_compositor_commands():
+def _get_compositor_commands() -> Tuple[Optional[List[str]], Optional[List[str]], bool]:
     """Returns the commands to enable/disable compositing on the current
     desktop environment as a 3-tuple: start command, stop-command and
     a flag to indicate if we need to run the commands in the background.
@@ -293,15 +298,15 @@ def _get_compositor_commands():
                 break
 
     if command_set:
-        start_compositor = command_set["start_compositor"]
-        stop_compositor = command_set["stop_compositor"]
+        start_compositor: List[str] = command_set["start_compositor"]
+        stop_compositor: List[str] = command_set["stop_compositor"]
         run_in_background = bool(command_set.get("run_in_background"))
         return start_compositor, stop_compositor, run_in_background
 
     return None, None, False
 
 
-def _run_command(*command, run_in_background=False):
+def _run_command(*command: str, run_in_background: bool = False) -> Optional[subprocess.Popen[bytes]]:
     """Random _run_command lost in the middle of the project,
     are you lost little _run_command?
     """
@@ -319,8 +324,10 @@ def _run_command(*command, run_in_background=False):
         errorMessage = "FileNotFoundError when running command:", command
         logger.error(errorMessage)
 
+    return None
 
-def disable_compositing():
+
+def disable_compositing() -> None:
     """Disable compositing if not already disabled."""
     compositing_enabled = is_compositing_enabled()
     if any(_COMPOSITING_DISABLED_STACK):
@@ -333,7 +340,7 @@ def disable_compositing():
         _run_command(*stop_compositor, run_in_background=background)
 
 
-def enable_compositing():
+def enable_compositing() -> None:
     """Re-enable compositing if the corresponding call to disable_compositing
     disabled it."""
 
@@ -355,18 +362,18 @@ class DBusScreenSaverInhibitor:
     org.gnome.ScreenSaver interfaces one can declare a DBus interface which
     requires the Inhibit() and UnInhibit() methods to be exposed."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.proxy = None
         self._used_gtk_fallback = False
 
-    def set_dbus_iface(self, name, path, interface, bus_type=Gio.BusType.SESSION):
+    def set_dbus_iface(self, name: str, path: str, interface: str, bus_type: Gio.BusType = Gio.BusType.SESSION) -> None:
         """Sets a dbus proxy to be used instead of Gtk.Application methods, this
         method can raise an exception."""
         self.proxy = Gio.DBusProxy.new_for_bus_sync(
             bus_type, Gio.DBusProxyFlags.NONE, None, name, path, interface, None
         )
 
-    def inhibit(self, game_name):
+    def inhibit(self, game_name: str) -> Optional[int]:
         """Inhibit suspend.
         Returns a cookie that must be passed to the corresponding uninhibit() call.
         If an error occurs, None is returned instead."""
@@ -384,7 +391,8 @@ class DBusScreenSaverInhibitor:
                 logger.warning("Failed to inhibit screensaver via D-Bus, falling back to GTK: %s", ex)
 
         # GTK fallback
-        app = Gio.Application.get_default()
+        app: "LutrisApplication" = Gio.Application.get_default()
+
         window = app.window
         flags = Gtk.ApplicationInhibitFlags.SUSPEND | Gtk.ApplicationInhibitFlags.IDLE
         cookie = app.inhibit(window, flags, reason)
@@ -396,7 +404,7 @@ class DBusScreenSaverInhibitor:
         self._used_gtk_fallback = True
         return cookie
 
-    def uninhibit(self, cookie):
+    def uninhibit(self, cookie: Optional[int]) -> None:
         """Uninhibit suspend.
         Takes a cookie as returned by inhibit. If cookie is None, no action is taken."""
         if not cookie:
@@ -410,11 +418,11 @@ class DBusScreenSaverInhibitor:
                 logger.warning("Failed to uninhibit screensaver via D-Bus: %s", ex)
 
         # GTK fallback
-        app = Gio.Application.get_default()
+        app: "LutrisApplication" = Gio.Application.get_default()
         app.uninhibit(cookie)
 
 
-def _get_suspend_inhibitor():
+def _get_suspend_inhibitor() -> DBusScreenSaverInhibitor:
     """Return the appropriate suspend inhibitor instance.
     If the required interface isn't available, it will default to GTK's
     implementation."""

lutris/config.py
@@ -3,7 +3,7 @@
 import os
 import time
 from shutil import copyfile
-from typing import Any, Dict, Optional, Set, TypeAlias
+from typing import Any, Dict, List, Optional, Set, TypeAlias
 
 from lutris import settings, sysoptions
 from lutris.runners import InvalidRunnerError, import_runner
@@ -286,7 +286,7 @@ class LutrisConfig:
     def options_as_dict(self, options_type: str) -> Dict[str, Any]:
         """Convert the option list to a dict with option name as keys"""
         if options_type == "system":
-            options = (
+            options: List[Dict[str, Any]] = (
                 sysoptions.with_runner_overrides(self.runner_slug) if self.runner_slug else sysoptions.system_options
             )
         else:
@@ -302,5 +302,5 @@ class LutrisConfig:
                 if not getattr(runner, attribute_name):
                     runner = runner()
 
-                options = getattr(runner, attribute_name)
+                options: List[Dict[str, Any]] = getattr(runner, attribute_name)
         return dict((opt["option"], opt) for opt in options)

lutris/game.py
@@ -747,7 +747,7 @@ class Game:
         if self.runner.system_config.get("display") != "off":
             self.resolution_changed = self.restrict_to_display(self.runner.system_config.get("display") or "")
 
-        resolution = self.runner.system_config.get("resolution")
+        resolution: str = self.runner.system_config.get("resolution")
         if resolution != "off":
             DISPLAY_MANAGER.set_resolution(resolution)
             time.sleep(3)

lutris/util/graphics/displayconfig.py
@@ -652,7 +652,7 @@ class MutterDisplayManager:
             return str(DEFAULT_RESOLUTION_WIDTH), str(DEFAULT_RESOLUTION_HEIGHT)
         return str(current_mode.width), str(current_mode.height)
 
-    def set_resolution(self, resolution: Union[str, List[DisplayConfig]]) -> None:
+    def set_resolution(self, resolution: Optional[Union[str, List[DisplayConfig]]]) -> None:
         """Change the current resolution"""
         if isinstance(resolution, str):
             monitor = self.display_config.get_primary_monitor()

lutris/util/graphics/xrandr.py
@@ -3,7 +3,7 @@
 import re
 import subprocess
 from collections import namedtuple
-from typing import Iterable, List, Tuple, Union
+from typing import Iterable, List, Optional, Tuple, Union
 
 from lutris.settings import DEFAULT_RESOLUTION_HEIGHT, DEFAULT_RESOLUTION_WIDTH
 from lutris.util.linux import LINUX_SYSTEM
@@ -119,7 +119,7 @@ def get_resolutions() -> List[str]:
     return sorted(set(resolution_list), key=lambda x: int(x.split("x")[0]), reverse=True)
 
 
-def change_resolution(resolution: Union[str, Iterable[Output]]) -> None:
+def change_resolution(resolution: Optional[Union[str, Iterable[Output]]]) -> None:
     """Change display resolution.
 
     Takes a string for single monitors or a list of displays as returned
@@ -206,7 +206,7 @@ class LegacyDisplayManager:  # pylint: disable=too-few-public-methods
         return tuple(mode.split("x"))
 
     @staticmethod
-    def set_resolution(resolution: Union[str, Iterable[Output]]) -> None:
+    def set_resolution(resolution: Optional[Union[str, Iterable[Output]]]) -> None:
         """Change the current resolution"""
         change_resolution(resolution)
 

commit 78d4fc38ea4e5ede4c032bbee4a3699b7d6b1452

fix: quote GnomeDesktop.RROutput annotation to prevent AttributeError
The type annotation `-> Optional[GnomeDesktop.RROutput]` on
DisplayManager._get_primary_output() is evaluated at class definition
time. When the GnomeDesktop 3.0 GIR typelib is not installed (common on
modern distros shipping only GnomeDesktop 4.0), GnomeDesktop is set to
None by the guarded import, causing:

    AttributeError: 'NoneType' object has no attribute 'RROutput'

This crashes Lutris at startup during module import and was introduced
in commit 0c88229ac ("mypy: add typing annotations to util/display.py").

Fix by quoting the annotation so it becomes a string literal that is
not resolved at runtime.

Also adds AST-based regression tests that verify no unquoted references
to conditionally-imported modules appear in type annotations.

lutris/util/display.py
@@ -76,7 +76,7 @@ class DisplayManager:
             return ["%sx%s" % (DEFAULT_RESOLUTION_WIDTH, DEFAULT_RESOLUTION_HEIGHT)]
         return sorted(set(resolutions), key=lambda x: int(x.split("x")[0]), reverse=True)
 
-    def _get_primary_output(self) -> Optional[GnomeDesktop.RROutput]:
+    def _get_primary_output(self) -> "Optional[GnomeDesktop.RROutput]":
         """Return the RROutput used as a primary display"""
         for output in self.rr_screen.list_outputs():
             if output.get_is_primary():

tests/util/test_display.py
@@ -1,0 +1,122 @@
+"""Tests for lutris.util.display module, particularly annotation safety
+when optional dependencies like GnomeDesktop are unavailable."""
+
+import ast
+from pathlib import Path
+from unittest import TestCase
+
+
+class TestDisplayAnnotationSafety(TestCase):
+    """Verify that type annotations in display.py don't reference
+    conditionally-imported modules in a way that would fail at runtime.
+
+    Commit 0c88229ac added a bare type annotation
+    `-> Optional[GnomeDesktop.RROutput]` which is evaluated at class
+    definition time. When GnomeDesktop is None (library unavailable),
+    this causes an AttributeError that prevents Lutris from launching.
+
+    The fix is to quote such annotations so they become string literals
+    and are not evaluated at runtime.
+    """
+
+    DISPLAY_MODULE_PATH = Path(__file__).resolve().parents[2] / "lutris" / "util" / "display.py"
+
+    # Modules that are conditionally imported and may be None at runtime
+    CONDITIONAL_MODULES = {"GnomeDesktop"}
+
+    def _parse_display_module(self):
+        source = self.DISPLAY_MODULE_PATH.read_text(encoding="utf-8")
+        return ast.parse(source, filename=str(self.DISPLAY_MODULE_PATH))
+
+    def _find_unquoted_refs_in_annotation(self, annotation, modules):
+        """Find references to conditional modules in an annotation AST node
+        that are NOT wrapped in a string (i.e., would be evaluated at runtime)."""
+        if annotation is None:
+            return []
+        if isinstance(annotation, ast.Constant) and isinstance(annotation.value, str):
+            # String annotation — safe, not evaluated at runtime
+            return []
+        if isinstance(annotation, ast.Name) and annotation.id in modules:
+            return [annotation.id]
+        if isinstance(annotation, ast.Attribute):
+            if isinstance(annotation.value, ast.Name) and annotation.value.id in modules:
+                return [f"{annotation.value.id}.{annotation.attr}"]
+        # Recurse into subscripts like Optional[GnomeDesktop.RROutput]
+        if isinstance(annotation, ast.Subscript):
+            refs = self._find_unquoted_refs_in_annotation(annotation.value, modules)
+            if isinstance(annotation.slice, ast.Tuple):
+                for elt in annotation.slice.elts:
+                    refs.extend(self._find_unquoted_refs_in_annotation(elt, modules))
+            else:
+                refs.extend(self._find_unquoted_refs_in_annotation(annotation.slice, modules))
+            return refs
+        if isinstance(annotation, ast.BinOp):
+            return self._find_unquoted_refs_in_annotation(
+                annotation.left, modules
+            ) + self._find_unquoted_refs_in_annotation(annotation.right, modules)
+        return []
+
+    def test_no_unquoted_conditional_module_annotations(self):
+        """All type annotations referencing conditionally-imported modules
+        (like GnomeDesktop) must be quoted strings to prevent AttributeError
+        when the module is unavailable."""
+        tree = self._parse_display_module()
+        errors = []
+
+        for node in ast.walk(tree):
+            if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
+                # Check return annotation
+                for ref in self._find_unquoted_refs_in_annotation(node.returns, self.CONDITIONAL_MODULES):
+                    errors.append(
+                        f"Line {node.returns.lineno}: unquoted annotation '{ref}' in return type of '{node.name}()'"
+                    )
+                # Check argument annotations
+                all_args = (
+                    node.args.posonlyargs
+                    + node.args.args
+                    + node.args.kwonlyargs
+                    + ([node.args.vararg] if node.args.vararg else [])
+                    + ([node.args.kwarg] if node.args.kwarg else [])
+                )
+                for arg in all_args:
+                    for ref in self._find_unquoted_refs_in_annotation(arg.annotation, self.CONDITIONAL_MODULES):
+                        errors.append(
+                            f"Line {arg.annotation.lineno}: unquoted annotation '{ref}' "
+                            f"in parameter '{arg.arg}' of '{node.name}()'"
+                        )
+            elif isinstance(node, ast.AnnAssign):
+                for ref in self._find_unquoted_refs_in_annotation(node.annotation, self.CONDITIONAL_MODULES):
+                    errors.append(f"Line {node.annotation.lineno}: unquoted annotation '{ref}' in variable annotation")
+
+        self.assertEqual(
+            errors, [], "Found unquoted annotations referencing conditionally-imported modules:\n" + "\n".join(errors)
+        )
+
+    def test_gnomedesktop_import_is_guarded(self):
+        """GnomeDesktop import must be wrapped in try/except with a fallback."""
+        tree = self._parse_display_module()
+
+        # Find the try/except that imports GnomeDesktop
+        found_guarded_import = False
+        found_fallback = False
+
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Try):
+                # Check if any handler sets GnomeDesktop = None or
+                # LIB_GNOME_DESKTOP_AVAILABLE = False
+                for handler in node.handlers:
+                    for child in ast.walk(handler):
+                        if isinstance(child, ast.Assign):
+                            for target in child.targets:
+                                if isinstance(target, ast.Name):
+                                    if target.id == "GnomeDesktop":
+                                        found_fallback = True
+                # Check if the try body imports GnomeDesktop
+                for child in ast.walk(node):
+                    if isinstance(child, ast.ImportFrom):
+                        for alias in child.names:
+                            if alias.name == "GnomeDesktop":
+                                found_guarded_import = True
+
+        self.assertTrue(found_guarded_import, "GnomeDesktop import should be inside a try block")
+        self.assertTrue(found_fallback, "GnomeDesktop import guard should set GnomeDesktop = None in except")